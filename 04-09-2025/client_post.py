f659adc317ddaa0e9c040cad97a5e31ce9b1348c
gmaheswaranmca <gmaheswaranmca@gmail.com>
2025-09-04 12:06:52 +0530

    API Consumption

--- 04-09-2025/client_post.py
+++ 04-09-2025/client_post.py
@@ -2,6 +2,22 @@
 
 import requests 
 baseUrl = 'https://jsonplaceholder.typicode.com'
+
+# read all posts : GET /posts 
+print('Consuming : Read All Posts...')
 response = requests.get(f'{baseUrl}/posts')
 posts = response.json()
 print(posts)
+
+# read by id : GET /posts/1
+print('Consuming : Read Post By Id == 1...')
+response = requests.get(f'{baseUrl}/posts/1')
+post = response.json()
+print(post)
+
+# create post : POST /posts {"userId":1, "title":"Some Title", "body" : "Some Body"}
+print('Consuming : create post...')
+post = {"userId":1, "title":"Some Title", "body" : "Some Body"}
+response = requests.post(f'{baseUrl}/posts', data = post)
+createdPost = response.json()
+print(createdPost)
