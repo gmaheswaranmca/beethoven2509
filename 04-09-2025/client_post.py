e8dd2908bbe550527078d179358c3058fdf25a19
gmaheswaranmca <gmaheswaranmca@gmail.com>
2025-09-04 12:15:21 +0530

    API Consumption

--- 04-09-2025/client_post.py
+++ 04-09-2025/client_post.py
@@ -21,3 +21,18 @@ post = {"userId":1, "title":"Some Title", "body" : "Some Body"}
 response = requests.post(f'{baseUrl}/posts', data = post)
 createdPost = response.json()
 print(createdPost)
+
+# update post : PUT /posts/1 {"userId":1, "title":"Some Title", "body" : "Some Body"}
+print('Consuming : update post...')
+new_post = {"userId":1, "title":"Some Title", "body" : "Some Body"}
+response = requests.put(f'{baseUrl}/posts/1', data = new_post)
+updatedPost = response.json()
+print(updatedPost)
+
+# delete post : DELETE /posts/1
+print('Consuming : delete post...')
+response = requests.delete(f'{baseUrl}/posts/1')
+if response.status_code == 200:
+    print('Post Deleted Successfully')
+else:
+    print('Error in deletiing the post')
